d8890d5ec60c991d68f484e5350472dec080c0d6
Akenax <camillexcamille@gmail.com>
2019-05-05 14:42:35 +0200

    partie avec map

---
+++ view/partie/index.php
@@ -0,0 +1,10 @@
+<body onload="reload();">
+	je suis là!<br>
+	<section id="truc"></section>
+	<?php
+		//echo $_SESSION["pseudo"];
+	?>
+</body>
+
+
+<script type="text/javascript" src="./js/partie.js"></script>
